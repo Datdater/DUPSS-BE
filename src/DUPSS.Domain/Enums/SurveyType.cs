8b08eefc9e7f68a013592e4f51c8d3e8594f361d
nctan123 <canhtancanhtan@gmail.com>
2025-06-23 22:34:20 +0700

    add migration AddSurverType

--- src/DUPSS.Domain/Enums/SurveyType.cs
+++ src/DUPSS.Domain/Enums/SurveyType.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace DUPSS.Domain.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum SurveyType
     {
-        None, 
-        PreWorkshop, 
-        PostWorkshop 
+        None,
+        PreWorkshop,
+        PostWorkshop
+    }
+
+    public static class SurveyTypeExtensions
+    {
+        public static string ToReadableString(this SurveyType surveyType)
+        {
+            return surveyType switch
+            {
+                SurveyType.PreWorkshop => "Pre-workshop Survey",
+                SurveyType.PostWorkshop => "Post-workshop Survey",
+                _ => surveyType.ToString()
+            };
+        }
     }
 }
