b83aa2d6633fdf274363aabe69e25da1b43ad056
doanhung <hungdmse173190@fpt.edu.vn>
2025-07-14 22:52:14 +0700

    Enhance user registration with new properties and types
    Updated `RegisterCommand` to include `UserName`, `Password`, `ConfirmPassword`, and `PhoneNumber`. Added `FirstName` and `LastName` properties, and changed `BirthDay` type to `DateOnly`.
    
    Refactored `RegisterCommandHandler` for improved readability and error handling. Adjusted `AppUser` to align with new property types.
    
    Created migration to update `BirthDay` column in the database and modified related files to reflect these changes.

--- src/DUPSS.Application/Features/Authentications/Commands/Register/RegisterCommand.cs
+++ src/DUPSS.Application/Features/Authentications/Commands/Register/RegisterCommand.cs
@@ -1,28 +1,30 @@
-﻿using DUPSS.Domain.Abstractions.Message;
-using DUPSS.Domain.Enums;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Data;
+using DUPSS.Domain.Abstractions.Message;
+using DUPSS.Domain.Enums;
 
 namespace DUPSS.Application.Features.Authentications.Commands.Register
 {
-	public class RegisterCommand : ICommand
-	{
-		public required string Email { get; set; }
+    public class RegisterCommand : ICommand
+    {
+        public required string Email { get; set; }
 
-		public required string UserName { get; set; }
-		public required string Password { get; set; }
-		public required string ConfirmPassword { get; set; }
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
 
-		[StringLength(100, MinimumLength = 2)]
-		public string? FirstName { get; set; }
+        [StringLength(100, MinimumLength = 2)]
+        public string? FirstName { get; set; }
 
-		[StringLength(100, MinimumLength = 2)]
-		public string? LastName { get; set; }
+        [StringLength(100, MinimumLength = 2)]
+        public string? LastName { get; set; }
 
-		[Phone]
-		public required string PhoneNumber { get; set; }
-		public required bool Gender { get; set; }
+        [Phone]
+        public required string PhoneNumber { get; set; }
 
-		public required Role Role { get; set; }
-	}
+        public DateOnly BirthDay { get; set; }
+        public required bool Gender { get; set; }
+
+        public required Role Role { get; set; }
+    }
 }
